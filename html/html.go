b3d6edc9659b72f74ffa021f22f5421799958515
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-08 15:10:49 -0300

    Corrigindo avisos

--- html/html.go
+++ html/html.go
@@ -1,13 +1,13 @@
-package main
+package html
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"regexp"
 )
 
-func titulo(urls ...string) <-chan string {
+// Titulo obtem o título de uma pagina html
+func Titulo(urls ...string) <-chan string {
 	c := make(chan string)
 	for _, url := range urls {
 		go func(url string) {
@@ -30,11 +30,4 @@ func titulo(urls ...string) <-chan string {
 		}(url)
 	}
 	return c
-}
-
-func main() {
-	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
-	t2 := titulo("https://www.amazon.com", "https://www.youtube.com")
-	fmt.Println("Primeiros:", <-t1, "|", <-t2)
-	fmt.Println("Segundos:", <-t1, "|", <-t2)
 }
