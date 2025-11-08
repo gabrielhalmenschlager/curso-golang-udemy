fa33beea571a80df36bcaa6a0f82c25dee49e3ee
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-08 15:20:54 -0300

    Padrão de Concorrência: Multiplexador

---
+++ concorrencia/multiplexar/multiplexar.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gabrielhalmenschlager/curso-golang-udemy/html"
+)
+
+func encaminhar(origem <-chan string, destino chan string) {
+	for {
+		destino <- <-origem
+	}
+}
+
+// Multiplexar - misturar (mensagens) num canal
+func juntar(entrada1, entrada2 <-chan string) <-chan string {
+	c := make(chan string)
+	go encaminhar(entrada1, c)
+	go encaminhar(entrada2, c)
+	return c
+}
+
+func main() {
+	c := juntar(
+		html.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		html.Titulo("https://chatgpt.com/", "https://www.youtube.com"),
+	)
+	fmt.Println(<-c, "|", <-c)
+	fmt.Println(<-c, "|", <-c)
+}
